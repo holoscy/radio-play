3e7ccad35d73ed03ea20ee4577d0bdd4150c99dd
darkduck9 <60059377+darkduck9@users.noreply.github.com>
2024-05-03 12:56:41 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 importScripts('https://cdn.bootcdn.net/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
 
-if (workbox) {
-    console.log('Workbox is loaded');
+const version = 'v0.2';
 
+if (workbox) {
     workbox.routing.registerRoute(
         ({request}) => request.destination === 'image',
         new workbox.strategies.CacheFirst({
-            cacheName: 'images-cache',
+            cacheName: `images-cache-${version}`,
             plugins: [
                 new workbox.expiration.ExpirationPlugin({
-                    maxEntries: 10000,
+                    maxEntries: 6000,
                     maxAgeSeconds: 30 * 24 * 60 * 60,
                 }),
                 new workbox.cacheableResponse.CacheableResponsePlugin({
-                    statuses: [0, 200],
+                    statuses: [200],
                 })
             ]
         })
@@ -22,14 +22,30 @@ if (workbox) {
     workbox.routing.registerRoute(
         ({request}) => request.destination === 'script',
         new workbox.strategies.CacheFirst({
-            cacheName: 'js-cache',
+            cacheName: `js-cache-${version}`,
             plugins: [
                 new workbox.expiration.ExpirationPlugin({
                     maxEntries: 20,
                     maxAgeSeconds: 30 * 24 * 60 * 60,
                 }),
                 new workbox.cacheableResponse.CacheableResponsePlugin({
-                    statuses: [0, 200],
+                    statuses: [200],
+                })
+            ]
+        })
+    );
+
+    workbox.routing.registerRoute(
+        ({request}) => request.destination === 'style',
+        new workbox.strategies.CacheFirst({
+            cacheName: `css-cache-${version}`,
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: 10,
+                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [200],
                 })
             ]
         })
@@ -38,5 +54,13 @@ if (workbox) {
 } else {
     console.log('Workbox failed to load');
 }
-
-//new workbox.strategies.NetworkFirst()
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => !cacheName.endsWith(version))
+                .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
