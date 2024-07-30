a19044bd770d275b57c24b6f5c17f4857e4b6057
darkduck9 <60059377+darkduck9@users.noreply.github.com>
2024-07-30 20:54:15 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-s
 
 const versions = {
     images: 'v0.52',
-    js: 'v0.53',
-    css: 'v0.52'
+    js: 'v0.54',
+    css: 'v0.53'
 };
 
 if (workbox) {
@@ -62,9 +62,15 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter(cacheName => {
-                    return !Object.values(versions).some(version => cacheName.endsWith(version));
-                }).map(cacheName => caches.delete(cacheName))
+                cacheNames.map(cacheName => {
+                    for (const [resourceType, version] of Object.entries(versions)) {
+                        const cachePrefix = `${resourceType}-cache-`;
+                        if (cacheName.startsWith(cachePrefix) && cacheName !== `${cachePrefix}${version}`) {
+                            console.log(`Deleting old cache: ${cacheName}`);
+                            return caches.delete(cacheName);
+                        }
+                    }
+                }).filter(Boolean) 
             );
         })
     );
